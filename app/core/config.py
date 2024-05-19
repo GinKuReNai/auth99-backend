b46b4f2781de39148e1fa662bda26e5ae79f5b51
Akito Koga <akito121215@gmail.com>
2024-05-19 14:01:14 +0900

    feat :sparkles: 環境変数等の設定ファイルを追加

---
+++ app/core/config.py
@@ -0,0 +1,35 @@
+import secrets
+from typing import Literal
+
+from pydantic import computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_ignore_empty=True,
+    )
+
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8days
+    DOMAIN: str = "localhost"
+    ENVIRONMENT: Literal["local", "develop", "test", "staging", "production"] = "local"
+
+    @computed_field
+    @property
+    def server_host(self) -> str:
+        if self.ENVIRONMENT == "local":
+            return f"http://{self.DOMAIN}"
+        return f"https://{self.DOMAIN}"
+
+    PROJECT_NAME: str
+
+    MYSQL_SERVER: str
+    MYSQL_PORT: int = 3306
+    MYSQL_USER: str
+    MYSQL_PASSWORD: str
+    MYSQL_DB: str = ""
+
+
+settings = Settings()
